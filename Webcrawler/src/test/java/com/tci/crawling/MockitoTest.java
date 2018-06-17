a630486e15ed9e455738a6327480a88dad0ff30c
Simon Onumajuru <sonumajuru@gmail.com>
2018-06-17 17:21:51 +0200

    hehe

--- Webcrawler/src/test/java/com/tci/crawling/MockitoTest.java
+++ Webcrawler/src/test/java/com/tci/crawling/MockitoTest.java
@@ -113,6 +113,9 @@ public class MockitoTest {
         Mockito.verify(mockedMusic).getName();
 
         assertEquals(null, mockedMusic.getName());
+
+        assertEquals(null, mockedMusic.getArtist());
+
     }
 
 
